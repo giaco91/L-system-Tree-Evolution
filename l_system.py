fixed bug in moment calculation

l_system.py
@@ -238,7 +238,7 @@ class Tree_interpreter():
 class Depth_specific_tree_interpreter():
 	#this interpreter turns a string with the standard vocabulary {'X','F','-','+','[',']'} into a set of line segments by the 
 	#rule of turtle walk. However, in this depth specific case the length of the move "F" and the angles "+/-" can depend on the iteration depth
-	def __init__(self,depth=2,plus_angles=[np.pi/8,np.pi/8],minus_angles=[-np.pi/8,np.pi/8],lengths=[1,1],cross_sections=[0.1,0.1],leaf_radius=0.2,leaf_density=0.01):
+	def __init__(self,depth=2,plus_angles=[np.pi/8,np.pi/8],minus_angles=[-np.pi/8,np.pi/8],lengths=[1,1],cross_sections=[0.1,0.1],leaf_radius=0.2,leaf_density=0):
 		if depth!=len(plus_angles) or depth!=len(minus_angles) or depth!=len(lengths) or depth!=len(cross_sections):
 			raise ValueError('the depth must be in accordance with the number of the depth parameters.')
 		self.depth=depth
@@ -311,24 +311,29 @@ class Depth_specific_tree_interpreter():
 				i+=n
 				L,M=get_reduced_moment(cm)
 				for j in range(len(branches)):
+					# print(branches[j].moment)
 					branches[j].add_moment(M*(L-branches[j].sc[0]))
+					# print(branches[j].moment)
 				if len(additional_branches)>0:
 					last_rec=True
 					side_branches+=additional_branches
 			elif w[i]==']':
-				# if len(branches)>0 and len(side_branches)==0:
 				if len(branches)>0 and not last_rec:
 					branches[-1].add_leaf(self.leaf_radius,self.leaf_mass)
+					cumulative_moment.append([branches[-1].ec[0]*self.leaf_mass,self.leaf_mass])
 					for j in range(len(branches)):
+						# print(branches[-1-j].moment)
 						branches[-1-j].add_moment(branches[-1].leaf_mass*(branches[-1].ec[0]-branches[-1-j].sc[0]))
-					#branches[-1].leaf_radius=self.leaf_radius
+						# print(branches[-1-j].moment)
 				return branches+side_branches,i+1,cumulative_moment
 			elif w[i]=='F':
 				last_rec=False
 				next_root=current_root+self.lengths[depth_list[i]]*rotation(current_angle,self.ex)
 				branch=Branch(self.cs[depth_list[i]],current_root,next_root)
 				for j in range(len(branches)):
+					# print(branches[j].moment)
 					branches[j].add_moment(branch.mass*(branch.center_of_mass_coordinates[0]-branches[j].sc[0]))
+					# print(branches[j].moment)
 				cumulative_moment.append([branch.center_of_mass_coordinates[0]*branch.mass,branch.mass])
 				branches.append(branch)
 				current_root=next_root
@@ -338,10 +343,14 @@ class Depth_specific_tree_interpreter():
 				current_angle+=self.m_angles[depth_list[i]]
 			i+=1
 		if len(branches)>0 and not last_rec:
+			# print('add leaf')
+			
 			branches[-1].add_leaf(self.leaf_radius,self.leaf_mass)
+
 			for j in range(len(branches)):
+				# print(branches[-1-j].moment)
 				branches[-1-j].add_moment(branches[-1].leaf_mass*(branches[-1].ec[0]-branches[-1-j].sc[0]))
-			#branches[-1].leaf_radius=self.leaf_radius
+				# print(branches[-1-j].moment)
 		return branches+side_branches,length_w,cumulative_moment
 
 class Branch():

test.py
@@ -12,27 +12,22 @@ from utils import *
 
 
 
-l_system=L_system()
-l_system.update_vocabulary({'X','F','-','+','[',']'})
+# l_system=L_system()
+# l_system.update_vocabulary({'X','F','-','+','[',']'})
 # l_system.add_rule('X', 'F+[[X]-X]-F[-FX]+X',verbose=True)
-l_system.add_rule('X', 'F[-X]+F',verbose=True)
-
-# l_system.add_rule('X', 'F+[[X]-X]',verbose=True)
-# print(l_system.rule['X'])
-# l_system.perm_mutation('X',1)
-# print(l_system.rule['X'])
+# # l_system.add_rule('X', '[-FX]',verbose=True)
+# # l_system.add_rule('X', '-FX',verbose=True)
 # depth=3
-# l_system.add_rule('X'+str(depth), 'F[X]',verbose=True)
-# print(l_system.rule)
-# TI=Depth_specific_tree_interpreter(depth=depth,plus_angles=[np.pi/8,np.pi/5,np.pi/5],minus_angles=[-np.pi/8,-np.pi/6,-np.pi/5],lengths=[1,1,1],cross_sections=[0.2,0.1,0.05],leaf_density=1)
-# # TI.modify_depth(3)
-# ls_list,ti_list=mutation([l_system],[TI],n_mut=2,leaf_specific=True,max_character=15)
-# print(ls_list[0].rule)
-# print(ls_list[1].rule)
+# # l_system.add_rule('X'+str(depth), 'F[X]',verbose=True)
 
-# w,dl=l_system.evolution('X',3)
-# b=TI.render(w,dl)
+# TI=Depth_specific_tree_interpreter(depth=depth,plus_angles=[np.pi/8,np.pi/5,np.pi/5],minus_angles=[-np.pi/8,-np.pi/6,-np.pi/5],lengths=[1,2,1],cross_sections=[0.01,0.1,0.01],leaf_density=0.01)
+# new_depth=5
+# TI.modify_depth(new_depth)
+# ls_list,ti_list=mutation([l_system],[TI],n_mut=2,leaf_specific=False,max_character=15)
+
+# w,dl=l_system.evolution('X',new_depth)
 # print(w)
+# b=TI.render(w,dl)
 # draw_branches(b,im_size=2000).show()
 
 # raise ValueError('asdf')
@@ -40,18 +35,21 @@ l_system.add_rule('X', 'F[-X]+F',verbose=True)
 for u in range(1):
 	gif_images=[]
 	depth=2
-	n_gen=60
-	depth_specific=True
+	max_depth=3
+	n_gen=50
+	leaf_specific=False
+	depth_specific=leaf_specific
 	l_system=L_system()
 	l_system.update_vocabulary({'X','F','-','+','[',']'})
 	init_w=l_system.sample_word(extension={'[X]','[F]'},immune={'[',']'},length=4)
 	init_w_depth=l_system.sample_word(extension={'[X]','[F]'},immune={'[',']'},length=4)
 	l_system.add_rule('X', init_w,verbose=True)
-	l_system.add_rule('X'+str(depth), init_w,verbose=True)
+	if leaf_specific:
+		l_system.add_rule('X'+str(depth), init_w,verbose=True)
 	TI=Depth_specific_tree_interpreter(depth=depth,plus_angles=[np.pi/8,np.pi/5],minus_angles=[-np.pi/8,-np.pi/6],lengths=[1,1],cross_sections=[0.1,0.1],leaf_radius=0.2,leaf_density=0.01)
 	n_trees=100
-	n_sel=10
-	ls_list,ti_list=mutation([l_system],[TI],n_mut=n_trees,leaf_specific=True,max_character=15)
+	n_sel=5
+	ls_list,ti_list=mutation([l_system],[TI],n_mut=n_trees,leaf_specific=leaf_specific,max_character=15,max_depth=max_depth)
 	for g in range(n_gen):
 		trees=[]
 		w,dl=l_system.evolution('X',ti_list[0].depth,depth_specific=depth_specific)
@@ -65,7 +63,7 @@ for u in range(1):
 			trees.append(branches)
 
 		selected_ls, selected_ti,max_score,best_branches=selection(ls_list,ti_list,n_sel=n_sel,depth_specific=depth_specific)
-		ls_list,ti_list=mutation(selected_ls,selected_ti,n_mut=n_trees,leaf_specific=True,max_character=15)
+		ls_list,ti_list=mutation(selected_ls,selected_ti,n_mut=n_trees,leaf_specific=leaf_specific,max_character=15,max_depth=max_depth)
 		best_ls=selected_ls[0]
 		best_ti=selected_ti[0]
 		
@@ -79,13 +77,13 @@ for u in range(1):
 			best_score=max_score
 			get_score(best_branches,best_ti,w,dl,verbose=True)
 			#im=draw_trees_from_branches(trees,im_size=3000)
-			#im.save('images/branch_tree_gen='+str(g)+'_score='+str(best_score)[0:4]+'.png')
-			print(best_ls.rule)
-			im_best=draw_branches(best_branches,im_size=500,text=['generation: '+str(g),'score: '+str(best_score)[0:5], 'L-gene: '+str(best_ls.rule['X'])])
+			#im.save('images/branch_tree_gen='+str(g)+'_score='+str(best_score)[0:4]+'.png'))
+			text=['generation: '+str(g),'score: '+str(best_score)[0:5], 'L-gene: '+str(best_ls.rule['X']), 'leaf radius: '+str(best_ti.leaf_radius)[0:5]]
+			im_best=draw_branches(best_branches,im_size=500,text=text)
 			#im_best.save('images/best_tree_gen='+str(g)+'_score='+str(best_score)[0:5]+'.png')
 			gif_images.append(im_best)
 		if g==n_gen-1:
-			text=['generation: '+str(g),'score: '+str(best_score)[0:5], 'L-gene: '+str(best_ls.rule['X'])]
+			text=['generation: '+str(g),'score: '+str(best_score)[0:5], 'L-gene: '+str(best_ls.rule['X']), 'leaf radius: '+str(best_ti.leaf_radius)[0:5]]
 			im_best=draw_branches(best_branches,im_size=500,text=text)
 			for i in range(10):
 				gif_images.append(im_best)

utils.py
@@ -201,7 +201,7 @@ def draw_trees(trees,im_size=1000,width=1):
 
 
 
-def draw_branches(branches,im_size=1000,text=None,draw_leafs=True,draw_stress=True):
+def draw_branches(branches,im_size=1000,text=None,draw_leafs=True,draw_stress=True, max_stress_only=True):
 	#text must be alist of strings
 	max_stress=get_max_stress(branches)
 	im=create_image(int(1.1*im_size),im_size)
@@ -220,10 +220,19 @@ def draw_branches(branches,im_size=1000,text=None,draw_leafs=True,draw_stress=Tr
 		#print('draw tree...')
 		for i in range(len(branches)):
 			if draw_stress:
-				if branches[i].stress()>0:
-					draw.line([tuple(bias+scale*branches[i].sc),tuple(bias+scale*branches[i].ec)],fill=(int(255*branches[i].stress()/max_stress),0,0),width=max(1,int(round(scale*branches[i].cs))))
+				if max_stress_only:
+					if abs(branches[i].stress())==max_stress:
+						if branches[i].stress()>0:
+							draw.line([tuple(bias+scale*branches[i].sc),tuple(bias+scale*branches[i].ec)],fill=(int(255*branches[i].stress()/max_stress),0,0),width=max(1,int(round(scale*branches[i].cs))))
+						else:
+							draw.line([tuple(bias+scale*branches[i].sc),tuple(bias+scale*branches[i].ec)],fill=(0,0,int(-255*branches[i].stress()/max_stress)),width=max(1,int(round(scale*branches[i].cs))))
+					else:
+						draw.line([tuple(bias+scale*branches[i].sc),tuple(bias+scale*branches[i].ec)],fill=(0,0,0),width=max(1,int(round(scale*branches[i].cs))))
 				else:
-					draw.line([tuple(bias+scale*branches[i].sc),tuple(bias+scale*branches[i].ec)],fill=(0,0,int(-255*branches[i].stress()/max_stress)),width=max(1,int(round(scale*branches[i].cs))))
+					if branches[i].stress()>0:
+						draw.line([tuple(bias+scale*branches[i].sc),tuple(bias+scale*branches[i].ec)],fill=(int(255*branches[i].stress()/max_stress),0,0),width=max(1,int(round(scale*branches[i].cs))))
+					else:
+						draw.line([tuple(bias+scale*branches[i].sc),tuple(bias+scale*branches[i].ec)],fill=(0,0,int(-255*branches[i].stress()/max_stress)),width=max(1,int(round(scale*branches[i].cs))))
 			else:
 				draw.line([tuple(bias+scale*branches[i].sc),tuple(bias+scale*branches[i].ec)],fill=(0,0,0),width=max(1,int(round(scale*branches[i].cs))))
 			if draw_leafs:
@@ -375,7 +384,7 @@ def get_total_mass(branches):
 def get_score(branches,TI,w,dl,verbose=False):
 	#calculates the score for a given tree described by the given list of branches
 	dx,dy,x_mean,y_min,mean_y=from_branches_get_max_size(branches)
-	average_stress=get_average_stress(branches)
+	#average_stress=get_average_stress(branches)
 	max_stress=get_max_stress(branches)
 	left_branches=TI.render(w,dl,starting_root=np.array([0,0]),starting_angle=np.pi/2+np.pi/6)
 	right_branches=TI.render(w,dl,starting_root=np.array([0,0]),starting_angle=np.pi/2-np.pi/6)
@@ -387,14 +396,14 @@ def get_score(branches,TI,w,dl,verbose=False):
 	max_stress_right=get_max_stress(right_branches)
 
 	#wind_stress=0*average_stress_left+0*average_stress_right+0*max_stress_left+0*max_stress_right
-	wind_stress=max_stress_left**3+max_stress_right**3
+	wind_stress=np.exp(min(10,max_stress_left))+np.exp(min(10,max_stress_right))
 	mass=get_total_mass(branches)
 	#a=-10*np.exp(-0.1*dx*dy)#maximize area of tree up to saturation
 	#b=-0.1*(dy-3)**2#tree hight is optimal at 3
 	#c=-abs(x_mean)**2#symmetry
 	#h=0.15*len(branches)**0.5
 	ground=(10*min(y_min,0))**2+0.1*((10*min(y_min_left,0))**2+(10*min(y_min_right,0))**2)#no branches with negative y-coordinates
-	stress=average_stress+max_stress**3+wind_stress
+	stress=np.exp(min(10,max_stress))+wind_stress
 	
 	tot_p_a=0
 	N=10
@@ -404,12 +413,13 @@ def get_score(branches,TI,w,dl,verbose=False):
 		tot_p_a+=projected_area
 	average_sunlight=20*tot_p_a/N
 	#leaf_cost=n_leafs*(TI.leaf_radius**2)
-	leaf_cost=n_leafs*np.exp(6*TI.leaf_radius)#
-	energy_loss=-0.3*mass-leaf_cost
+	leaf_cost=n_leafs*np.exp(min(10,6*TI.leaf_radius))#
+	energy_loss=-mass-leaf_cost
 	#height=1*max(0,mean_y)**0.5
 	height=2*max(0,average_leaf_height)**0.5
 	score=-ground-stress+energy_loss+average_sunlight+height
 	if verbose:
+		print('mass loss: '+str(-mass))
 		print('leaf radius: '+str(TI.leaf_radius))
 		print('ground '+str(-ground))
 		print('stress: '+str(-stress))
@@ -472,7 +482,7 @@ def selection(ls_list,ti_list,n_sel=1,depth_specific=False):
 		selected_ti.append(ti_list[sorted_idx[-1-j]])
 	return selected_ls, selected_ti,scores[sorted_idx[-1]],trees[sorted_idx[-1]]
 
-def mutation(selected_ls,selected_ti,n_mut=10,letter=['X'],leaf_specific=False,max_character=15):
+def mutation(selected_ls,selected_ti,n_mut=10,letter=['X'],leaf_specific=False,max_character=15,max_depth=4):
 	L=len(selected_ti)
 	mut_ls=[]
 	mut_ti=[]
@@ -490,14 +500,14 @@ def mutation(selected_ls,selected_ti,n_mut=10,letter=['X'],leaf_specific=False,m
 			mut_i_ti=deepcopy(selected_ti[idx])
 			mut_i_ti.angle_and_size_mutation()
 			old_depth=mut_i_ti.depth
-			mut_i_ti.depth_mutation(p=0.1,max_depth=4,min_depth=2)
+			mut_i_ti.depth_mutation(p=0.1,max_depth=max_depth,min_depth=2)
 			new_depth=mut_i_ti.depth
 			if leaf_specific:
-				mut_i_ls.general_mutation('X',p=0.05,immune={'[',']'},words={'[X]','[F]'},max_character=max_character)
+				mut_i_ls.general_mutation('X',p=0.02,immune={'[',']'},words={'[X]','[F]'},max_character=max_character)
 				if old_depth!=new_depth:
 					mut_i_ls.add_rule('X'+str(new_depth), mut_i_ls.rule['X'+str(old_depth)])
 					mut_i_ls.rule.pop('X'+str(old_depth))
-				mut_i_ls.general_mutation('X'+str(new_depth),p=0.05,immune={'[',']'},words={'[X]','[F]'},max_character=max_character)
+				mut_i_ls.general_mutation('X'+str(new_depth),p=0.02,immune={'[',']'},words={'[X]','[F]'},max_character=max_character)
 			else:	
 				for l in letter:
 					mut_i_ls.general_mutation(l,p=0.05,immune={'[',']'},words={'[X]','[F]'},max_character=max_character)